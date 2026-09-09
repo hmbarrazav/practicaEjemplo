7701dacabb0808e4166cf0e16de1759183604f89
hmbarrazav <hmbarrazav@gmail.com>
2026-09-09 16:47:46 -0500

    Añadir mensajes al usuario

--- hola.cpp
+++ hola.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(){
     int a,b,c;
-    cin>>a;
-    cin>>b;
+    cout<<"Ingresa un numero: "; cin>>a;
+    cout<<"Ingresa otro numero: "; cin>>b;
     c=a+b;
-    cout<<c;
+    cout<<"La suma es: "<<c;
     return 0;
 }
